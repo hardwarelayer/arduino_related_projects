a18b06f89cd8f486a55c2f71332d1e03d9f419c6
TienTN <hardwarelayer@yahoo.com>
2023-03-14 15:19:51 +0700

    Add files via upload

---
+++ esp_wroom32_modbus_sys1/esp_wroom32_modbus_sys1.ino
@@ -0,0 +1,134 @@
+#include <bmps.h>
+#include <ST7565_homephone_esp8266.h>
+#include "DHT.h"
+#include <ModbusMaster.h>
+
+//The ESP32 has 3 different Serial Ports (UART)
+//Serial0: RX0 on GPIO3, TX0 on GPIO1
+//Serial1: RX1 on GPIO9, TX1 on GPIO10 (+CTS1 and RTS1)
+//Serial2: RX2 on GPIO16, TX2 on GPIO17 (+CTS2 and RTS2)
+
+#define DHTPIN 14     // Digital pin connected to the DHT sensor(D14)
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+#define LEDPIN 27
+#define LCDPIN 18
+
+ST7565 lcd(23,22,21,19);
+DHT dht(DHTPIN, DHTTYPE);
+
+#define RXD2 16
+#define TXD2 17
+ModbusMaster node;
+
+void setup()   {  
+  lcd.ON();//c
+  lcd.SET(22,0,0,0,4);   //d
+
+  dht.begin();
+
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(LCDPIN, OUTPUT);
+
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  node.begin(1, Serial2); //slave 1, Serial 8N1
+}
+
+bool isInited = false;
+char sHumidity[40];
+char sTemperature[40];
+char sHic[40];
+
+char sModbusMsg[40];
+bool isOdd = true;
+
+void loop(){
+
+  digitalWrite(LEDPIN, isOdd?HIGH:LOW);
+
+  if (!isInited) {
+    digitalWrite(LCDPIN, HIGH);
+ 
+    lcd.Asc_String(0, 0, "Welcome to CloudSky", BLACK);
+    lcd.Asc_String(0, 10, "Version 1.0", BLACK);
+    lcd.Asc_String(0, 20, "Initializing sensors ...", BLACK);
+    lcd.Display();
+  
+    delay(2000);
+    lcd.Clear();
+    lcd.Asc_String(0, 0, "Welcome to CloudSky", BLACK);
+    lcd.Asc_String(0, 10, "Version 1.0", BLACK);
+    lcd.Asc_String(0, 20, "Init completed!", BLACK);
+    lcd.Display();
+    delay(1000);
+
+    isInited = true;
+  }
+
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  lcd.Clear();
+  lcd.Asc_String(0, 0, "Welcome to CloudSky", BLACK);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  sprintf(sHumidity, "Humid: %2.2f", h);
+  sprintf(sTemperature, "Temp C/F:%2.2f/%2.2f", t, f);
+  sprintf(sHic, "HeatIdx C/F:%2.2f/%2.2f", hic, hif);
+  
+  lcd.Asc_String(0, 20, sHumidity, BLACK);
+  lcd.Asc_String(0, 30, sTemperature, BLACK);
+  lcd.Asc_String(0, 40, sHic, BLACK);
+  lcd.Display();
+
+  sprintf(sModbusMsg, "Turn %s the LEDs", isOdd?"on":"off");
+  lcd.Asc_String(0, 50, sModbusMsg, BLACK);
+  lcd.Display();
+
+  if (isOdd) {
+    Serial.println("Sending ON to MODBUS");
+    sprintf(sModbusMsg, "Turn %s LED1    ", isOdd?"on":"off");
+    lcd.Asc_String(0, 50, sModbusMsg, BLACK);
+    lcd.Display();
+    node.writeSingleRegister(0x40000, 1);               //Writes 1 to 0x40000 holding register
+
+    sprintf(sModbusMsg, "Turn %s LED2    ", isOdd?"on":"off");
+    lcd.Asc_String(0, 50, sModbusMsg, BLACK);
+    lcd.Display();
+    node.writeSingleRegister(0x40001, 1);               //Writes 1 to 0x40001 holding register
+
+    sprintf(sModbusMsg, "Turn %s BULB    ", isOdd?"on":"off");
+    lcd.Asc_String(0, 50, sModbusMsg, BLACK);
+    lcd.Display();
+    node.writeSingleRegister(0x40002, 1);               //Writes 1 to 0x40002 holding register
+  }
+  else {
+    Serial.println("Sending OFF to MODBUS");
+    node.writeSingleRegister(0x40000, 0);               //Writes 1 to 0x40000 holding register
+    node.writeSingleRegister(0x40001, 0);               //Writes 1 to 0x40001 holding register
+    node.writeSingleRegister(0x40002, 0);               //Writes 1 to 0x40002 holding register
+  }
+
+  delay(3000);
+
+  isOdd = !isOdd;
+}  
